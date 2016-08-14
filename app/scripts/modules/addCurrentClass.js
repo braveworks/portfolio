daa5d3c692371b53a9df1c3afd5ed85536db06ea
mun <mun@mun-no-MacBook-Pro.local>
2016-08-14 19:20:31 +0900

    addclass 処理修正

--- app/scripts/modules/addCurrentClass.js
+++ app/scripts/modules/addCurrentClass.js
@@ -3,18 +3,20 @@ var addCurrentClass = function() {
   var $gnav = $('.gnav');
   var url = location.pathname.split('/')[location.pathname.split('/').length - 1];
   url = (url) ? url : 'index.html';
-  console.log();
+  console.log(url);
 
   $gnav.find('a').removeClass('current');
   $gnav.find('a[href^="' + url + '"]').addClass('current');
 
-  var wh = $(window).height();
-  var ct = $gnav.find('.current').offset().top;
-  var cb = ct + $gnav.find('.current').outerHeight();
+  if ($('.current').length) {
+    var wh = $(window).height();
+    var ct = $gnav.find('.current').offset().top;
+    var cb = ct + $gnav.find('.current').outerHeight();
 
-  if (cb > wh || 0 > ct) {
-    var targetOffset = ct;
-    TweenMax.to($gnav, 3, { scrollTo: { y: targetOffset, autoKill: true, force3D: true }, ease: Power4.easeOut });
+    if (cb > wh || 0 > ct) {
+      var targetOffset = ct;
+      TweenMax.to($gnav, 3, { scrollTo: { y: targetOffset, autoKill: true, force3D: true }, ease: Power4.easeOut });
+    }
   }
 
 };
